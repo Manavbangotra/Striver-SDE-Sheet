98b944375c98c31a706aae7c7b7b75dc1a245dd6
Manav Bangotra <87271558+Manavbangotra@users.noreply.github.com>
2023-07-19 23:03:32 +0530

    Create 039_3sum.cpp

---
+++ 039_3sum.cpp
@@ -0,0 +1,29 @@
+//leetcode
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& nums)
+    {
+        sort(nums.begin(), nums.end());
+        vector<vector<int>> res;
+
+        int n = nums.size();
+        for (int i = 0; i < n - 2; i++) {
+            if (i == 0 || (i > 0 && nums[i] != nums[i - 1])) {
+                int lo = i + 1, hi = n - 1, sum = 0 - nums[i];
+
+                while (lo < hi) {
+                    if (nums[lo] + nums[hi] == sum) {
+                        res.push_back({nums[i], nums[lo], nums[hi]});
+
+                        while (lo < hi && nums[lo] == nums[lo + 1]) lo++;
+                        while (lo < hi && nums[hi] == nums[hi - 1]) hi--;
+
+                        lo++, hi--;
+                    } else if (nums[lo] + nums[hi] < sum) lo++;
+                    else hi--;
+                }
+            }
+        }
+        return res;
+    }
+};
