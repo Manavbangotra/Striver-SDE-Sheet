e799ec5e351707e558280abc2c2d4aaaa3e2dea0
Manav Bangotra <87271558+Manavbangotra@users.noreply.github.com>
2023-07-22 12:10:59 +0530

    Update 042_MaxConsecutiveOnes.cpp

--- 042_MaxConsecutiveOnes.cpp
+++ 042_MaxConsecutiveOnes.cpp
@@ -1,1 +1,16 @@
 //leetcode
+int findMaxConsecutiveOnes(vector<int>& nums) {
+        int count = 0;
+        int maxVal = 0;
+        
+        for(int i = 0; i<nums.size(); i++){
+            if(nums[i] == 0){
+                count = 0;
+            }
+            else{
+                count++;
+                maxVal = max(maxVal, count);
+            }
+        }
+        return maxVal;
+    }
